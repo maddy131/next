0d019b782cfe4ee9a54a4a30916de2bd35e4dc91
maddy131 <maitraee131@gmail.com>
2024-07-21 10:07:03 +0530

    till chapter 15 all done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -23,6 +23,7 @@ export async function fetchRevenue() {
      console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
+    
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
@@ -163,7 +164,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
